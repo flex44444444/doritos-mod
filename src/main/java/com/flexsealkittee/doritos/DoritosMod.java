43ee69950a79fc256838d527798df2d0d3472c4c
flex44444444 <flex44444444@gmail.com>
2022-08-19 21:12:50 -0400

    initial commit
    initial commit

---
+++ src/main/java/com/flexsealkittee/doritos/DoritosMod.java
@@ -0,0 +1,14 @@
+package com.flexsealkittee.doritos;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DoritosMod implements ModInitializer {
+
+    public static final String MOD_ID = "doritos";
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
